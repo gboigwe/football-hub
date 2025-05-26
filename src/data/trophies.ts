43714a78b5b2b33c56eaf19d0d2e718e77d30807
Gbolahan Akande <geakande@gmail.com>
2025-05-26 17:37:45 +0100

    Added trophies data component

---
+++ src/data/trophies.ts
@@ -0,0 +1,13 @@
+import { Trophy } from '../types';
+
+export const trophies: Trophy[] = [
+  {
+    id: "ucl-2008",
+    clubId: "man-utd",
+    name: "UEFA Champions League",
+    competition: "UEFA Champions League",
+    season: "2007/08",
+    description: "Victory against Chelsea in Moscow on penalties",
+    image: "https://images.pexels.com/photos/9750343/pexels-photo-9750343.jpeg"
+  }
+];
